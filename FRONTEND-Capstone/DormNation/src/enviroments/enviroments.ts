9cb3a4b7fd7804e83415f9e41bf44e070e49d84e
Baraa EL Ghalayini <74971527+Worldlyberry265@users.noreply.github.com>
2024-05-10 19:00:33 +0300

    Add files via upload

---
+++ FRONTEND-Capstone/DormNation/src/enviroments/enviroments.ts
@@ -0,0 +1,14 @@
+
+
+
+export const enviroment = {
+    production: false
+};
+
+//for localhost
+// export const baseUrl = 'http://localhost:5000/';
+
+//for AWS - Default port for http requests is 80 , for https its 443
+// export const baseUrl = 'https://DormnationServer.eu-west-3.elasticbeanstalk.com/';
+
+export const baseUrl = 'https://dormnationserver.link/';
